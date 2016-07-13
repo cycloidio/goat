f8a07272cf42915dc5b6008e3c4853b032987dcb
Hugo Rosnet <hugo.rosnet@cycloid.io>
2016-07-13 10:42:01 +0200

    auth0: Implementing first version of call for auth0
    The SDK is supposed to provide methods that can be called to be as
    simple to use as the Auth0 API we are wrapping. This adds the methods
    CRUD for users and review how the call are made to have something
    as generic as possible.
    
    Currently that will work with almost all Auth0 User's API methods,
    but if we want to implement all of their methods, we might have
    to slightly modify it, as they sometimes include extra 'directory'
    such as for:
    * Delete a user's multifactor provider
    * Unlink a user account
    * or Link a user account
    
    The methods for CRUD on users includes basic checks of nil/empty.
    Most of the code (94.1%) is also tested. We should be added as a hook
    ideally as well as go vet to have automatic check/coverage.

---
+++ auth0/call.go
@@ -0,0 +1,70 @@
+package goat
+
+import (
+	"net/http"
+	"io/ioutil"
+	"net"
+	"time"
+	"bytes"
+)
+
+const (
+	DELETE = "DELETE"
+	GET = "GET"
+	PATCH = "PATCH"
+	POST = "POST"
+)
+
+type Auth0 struct {
+	domain string
+	client *http.Client
+	api_url string
+	resp_status string
+	resp_headers map[string][]string
+	resp_body []byte
+	token string
+}
+
+func Auth0New(auth0_domain, api_version, token string) *Auth0 {
+	var netTransport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+
+	a := Auth0{}
+	a.client = &http.Client{
+		Timeout: time.Second * 10,
+		Transport: netTransport,
+	}
+	a.domain = auth0_domain
+	a.api_url = api_version
+	a.token = token
+	return &a
+}
+
+func (a *Auth0) Call(api_action string, method string, body []byte) ([]byte, error) {
+	var uri = a.domain + a.api_url + api_action
+
+	_, err := a.client.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(method, uri, bytes.NewBuffer(body))
+	req.Header.Add("Authorization", "Bearer " + a.token)
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	a.resp_status = resp.Status
+	a.resp_headers = resp.Header
+	res_body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	a.resp_body = res_body
+	return res_body, nil
+}
